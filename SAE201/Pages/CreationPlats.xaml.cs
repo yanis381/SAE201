3cd21ef7ca6fdae737693b8a4c4ba66e80630f49
Alexandre Savy <Alexandre.Savy@etu.univ-smb.fr>
2025-06-06 14:44:18 +0200

    creation pagepour la creation plat client commande

---
+++ SAE201/Pages/CreationPlats.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SAE201.Pages
+{
+    /// <summary>
+    /// Logique d'interaction pour CreationPlats.xaml
+    /// </summary>
+    public partial class CreationPlats : Page
+    {
+        public CreationPlats()
+        {
+            InitializeComponent();
+        }
+    }
+}
