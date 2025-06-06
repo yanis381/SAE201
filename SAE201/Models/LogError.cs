8b9e50895a5f9941e12aa5598d5159185eb863be
Alexandre Savy <Alexandre.Savy@etu.univ-smb.fr>
2025-06-06 14:51:20 +0200

    rajout logError.cs + relgage pb

---
+++ SAE201/Models/LogError.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TD3_BindingBDPension.Model
+{
+    public class LogError
+    {
+        public static void Log(Exception ex, string msg)
+        {
+            string logFile = "error.log";
+            string content = $"{DateTime.Now}:{msg}\n {ex.Message}\n{ex.StackTrace}\n";
+            try
+            {
+                File.AppendAllText(logFile, content);
+            }
+            catch
+            { }
+        }
+    }
+}
