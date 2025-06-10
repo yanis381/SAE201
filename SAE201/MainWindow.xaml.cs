a1ae7c43680d042b64d9abbbf969bba198cb9bfa
Yanis Debbal <Yanis.Debbal@etu.univ-smb.fr>
2025-06-10 15:58:05 +0200

    creation client font page connexion fini yes

--- SAE201/MainWindow.xaml.cs
+++ SAE201/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace SAE201
         public MainWindow()
         {
             InitializeComponent();
-            WindowsConnexions wind  = new WindowsConnexions();
+            PageConnexion wind  = new PageConnexion();
             wind.ShowDialog();
             //MainContent.Content = new Connexions();
             
